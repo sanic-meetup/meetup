ed9478ac2282ccc6761b25e65f98c750685be642
Daniel Persaud <dev.persaud@mail.utoronto.ca>
2017-03-05 21:31:37 -0500

    added conf for db connections

---
+++ server/api/conf.js
@@ -0,0 +1,8 @@
+//username = your_name, password = your_name123
+var username = "kiwi";
+var password = "kiwi123";
+
+var conf = {
+  mongouri: "mongodb://"+username+":"+password+"@ds119750.mlab.com:19750/freetime"
+}
+module.exports = conf;
