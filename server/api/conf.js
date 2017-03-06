c01237193e58c504a53fabc1a5d862e487cb977e
Daniel Persaud <dev.persaud@mail.utoronto.ca>
2017-03-05 22:07:57 -0500

    Delete conf.js

--- server/api/conf.js
+++
@@ -1,8 +0,0 @@
-//username = your_name, password = your_name123
-var username = "daniel";
-var password = "daniel123";
-
-var conf = {
-  mongouri: "mongodb://" + username + ":" + password + "@ds119750.mlab.com:19750/freetime"
-};
-module.exports = conf;
