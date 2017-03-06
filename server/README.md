ca96c9b63cba96911cae1b3f447499ce526a24f7
Daniel Persaud <dev.persaud@mail.utoronto.ca>
2017-03-05 20:55:41 -0500

    implemented token auth and user - signout method

--- server/README.md
+++ server/README.md
@@ -1,1 +1,39 @@
 # Server
+##Users API
+###Registering A New User
+- description: register a new user
+- request: `POST /api/users/`
+  - content-type: `application/json`
+  - body: object
+    - username: (string) the username
+    - password: (string) the password
+- response: 200 | 500 if server error | 409 if user already exists
+  - content-type: `application/json`
+  - body: object
+    - username: the username of new user
+
+```
+curl -X PUT -H "Content-Type: application/json" -H "Cache-Control: no-cache" -H "Postman-Token: 1c19fbae-267c-ddfe-6076-c7755ab01356" -d '{
+	"username": "test",
+	"password": "test"
+}' "https://localhost:3000/api/users"
+```
+
+###Logging In
+- description: register a new user
+- request: `POST /api/signin/`
+  - content-type: `application/json`
+  - body: object
+    - username: (string) the username
+    - password: (string) the password
+- response: 200 | 500 if server error | 401 if Unauthorized | 400 if bad req.
+  - content-type: `application/json`
+  - body: object
+    - username: (string) the username of user
+    - token: (string) the token used to authenticate in other methods
+    - expiresIn: (int) when the token expires in seconds
+
+###testing auth works
+``GET "/api/testauth"``
+send token using ?token="token" or body or header
+returns 200 if successful
---
+++ server/api/app.js
@@ -0,0 +1,136 @@
+/*jshint esversion:6*/
+var crypto = require('crypto');
+var path = require('path');
+var express = require('express');
+var app = express();
+var sanitizer = require('sanitizer');
+var validator = require('express-validator');
+var jwt = require('jsonwebtoken');
+
+//body parser stuff
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//validator
+app.use(validator([]));
+
+//db
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://daniel:daniel123@ds119750.mlab.com:19750/freetime');
+
+//models
+var User = require("./models/user");
+
+//for auth
+app.set('superSecret', 'this is a supersecret secret key'); // secret variable
+
+app.get("/", function(req, res, next) {
+  res.send("successfully connected to api");
+});
+
+//middleware to verify a token
+var apiRoutes = express.Router();
+apiRoutes.use(function (req, res, next) {
+  //this function is from the scoth.io tut - see the credits.md
+  // check header or url parameters or post parameters for token
+   var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+   // decode token
+   if (token) {
+     // verifies secret and checks exp
+     jwt.verify(token, app.get('superSecret'), function(err, decoded) {
+       if (err) {
+         return res.json({ success: false, message: 'Failed to authenticate token.' });
+       } else {
+         // if everything is good, save to request for use in other routes
+         req.decoded = decoded;
+         next();
+       }
+     });
+
+   } else {
+     // if there is no token
+     // return an error
+     return res.status(403).send({
+         success: false,
+         message: 'No token provided.'
+     });
+   }
+});
+
+//apply apiroutes only to routes with prefix /api
+app.use('/api', apiRoutes);
+
+app.post("/api/users/", function (req, res, next) {
+  //some basic validation
+  req.body.username = sanitizer.sanitize(req.body.username);
+  req.body.password = sanitizer.sanitize(req.body.password);
+  req.checkBody('username', 'username field cannot be empty').notEmpty();
+  req.checkBody('password', 'password field cannot be empty').notEmpty();
+
+  var new_user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  new_user.save(function(err) {
+    //if err user exists
+    if (err) res.status(409).send('new user not created');
+    else {
+      res.status(200).send({username:req.body.username});
+    }
+  });
+
+});
+
+var checkPassword = function(user, password){
+        var hash = crypto.createHmac('sha512', user.salt);
+        hash.update(password);
+        var value = hash.digest('base64');
+        return (user.password === value);
+};
+
+app.post('/api/signin/', function (req, res, next) {
+  //sanitize
+  req.body.username = sanitizer.sanitize(req.body.username);
+  req.body.password = sanitizer.sanitize(req.body.password);
+  if (!req.body.username || ! req.body.password) return res.status(400).send("Bad Request");
+
+  var user = new User({
+      username: req.body.username,
+      password: req.body.password
+    });
+
+  User.find({username: user.username}, function(err, result){
+    if (err) return res.status(500).end(err);
+    if (!result || !checkPassword(result[0], user.password)) return res.status(401).end("Unauthorized");
+
+    // successful auth,  create a token
+    var t = 60*60*24;
+    var token = jwt.sign(result[0], app.get('superSecret'), {
+      expiresIn: t // expires in 24 hours (measured in seconds)
+    });
+
+    res.status(200).send({username: user.username, token: token, expiesIn: t});
+  });
+});
+
+app.get("/api/testauth", function(req, res, next){
+    res.status(200).send("authorized");
+});
+
+//https setup
+var https = require("https");
+var fs = require('fs');
+var privateKey = fs.readFileSync( 'certs/server.key' );
+var certificate = fs.readFileSync( 'certs/server.crt' );
+var config = {
+        key: privateKey,
+        cert: certificate
+};
+
+//https server
+https.createServer(config, app).listen(3000, function () {
+    console.log('HTTPS on port 3000');
+});
