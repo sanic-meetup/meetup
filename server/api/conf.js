11be77d2fdd24b7ba2c9a41f170e3f5840cb6dde
Daniel Persaud <dev.persaud@mail.utoronto.ca>
2017-03-05 21:41:51 -0500

    Update conf.js

--- server/api/conf.js
+++ server/api/conf.js
@@ -1,5 +1,6 @@
 //username = your_name, password = your_name123
-var password = "daniel123";
+var username = "username";
+var password = "passwd";
 
 var conf = {
   mongouri: "mongodb://"+username+":"+password+"@ds119750.mlab.com:19750/freetime"
