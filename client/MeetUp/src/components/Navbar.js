12be914f66d412a6e67ea158154ff39025b960a5
Brandon Mowat <brandonmowat11@gmail.com>
2017-03-08 23:04:13 -0500

    📱 made a nice little navbar

---
+++ client/MeetUp/src/components/Navbar.js
@@ -0,0 +1,81 @@
+
+/**
+* My custom Navbar
+*/
+
+import React, { Component, Stylesheet } from 'react';
+import { Text, View } from 'react-native';
+import { Actions } from 'react-native-router-flux';
+
+const NAV_BAR_HEIGHT = 44;
+const STATUS_BAR_HEIGHT = 20;
+
+/**
+* Styles - basically stolen from react-native-navbar.
+* so credit where credit is due.
+*/
+const styles = {
+  navBarContainer: {
+    backgroundColor: 'white',
+  },
+  statusBar: {
+    height: STATUS_BAR_HEIGHT,
+  },
+  navBar: {
+    height: NAV_BAR_HEIGHT,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'stretch',
+  },
+  customTitle: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 7,
+    alignItems: 'center',
+  },
+  navBarButtonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'stretch',
+  },
+  navBarButton: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  navBarButtonText: {
+    fontSize: 17,
+    letterSpacing: 0.5,
+  },
+  navBarTitleContainer: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  navBarTitleText: {
+    fontSize: 17,
+    letterSpacing: 0.5,
+    color: '#333',
+    fontWeight: '500',
+  }
+};
+
+export default class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <View style={[styles.navBarContainer]}>
+      <View style={styles.statusBar}></View>
+      <View style={[styles.navBar]}>
+        <View style={styles.customTitle}><Text style={styles.navBarTitleText}>Title</Text></View>
+      </View>
+    </View>
+  }
+}
--- client/MeetUp/src/views/Login.js
+++ client/MeetUp/src/views/Login.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import Navbar from "../components/Navbar";
 
 export default class Login extends React.Component {
   render() {
     return(
-      <View>
-        <Text>Login</Text>
+      <View style={{flex: 1}}>
+        <Navbar/>
+        <Text>Test</Text>
       </View>
     );
   }
