f979663eb5387d8fb4ce942dcd111c661ea0e6c8
Kirisanth Ganesh <kirisanth.g@hotmail.com>
2017-03-10 16:33:30 -0500

    Added Following schema

---
+++ server/api/models/following.js
@@ -0,0 +1,18 @@
+// grab the things we need
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// create a schema
+var following_schema = new Schema({
+  username: { type: String, required: true, unique: true },
+  following: {type: Array},
+  created_at: Date,
+  updated_at: Date
+});
+
+// the schema is useless so far
+// we need to create a model using it
+var following = mongoose.model('following', following_schema);
+
+// make this available to our users in our Node applications
+module.exports = follower;
