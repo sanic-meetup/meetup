da37be5b21f0d2c800ceb59375a7e644d9a11fe8
Daniel Persaud <dev.persaud@mail.utoronto.ca>
2017-03-12 16:48:00 -0400

    added stat codes

---
+++ server/api/utils/utils.js
@@ -0,0 +1,6 @@
+exports.statcodes = {
+  _400: JSON.stringify({response:"bad request"}),
+  _401: JSON.stringify({response:"unauthorized"}),
+  _409: JSON.stringify({response:"new user not created"}),
+  _500: JSON.stringify({response:"server error"})
+};
