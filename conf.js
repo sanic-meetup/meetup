cd20f40ea3a08974163e9f27ed33d2379c0a6826
Daniel Persaud <dev.persaud@mail.utoronto.ca>
2017-03-19 21:57:44 -0400

    created conf.js for cms webserver only

---
+++ conf.js
@@ -0,0 +1,6 @@
+var username = 'cmsapp';
+var password = 'cmsapp';
+var conf = {
+mongouri: "mongodb://" + username + ":" + password + "@ds119750.mlab.com:19750/freetime"
+};
+module.exports = conf;
