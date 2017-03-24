9198057821b3575fcd68c2b52312928819067c04
Kirisanth Ganesh <kirisanth.g@hotmail.com>
2017-03-24 14:08:37 -0400

    Added 404 error msg

--- server/api/utils/utils.js
+++ server/api/utils/utils.js
@@ -7,6 +7,7 @@ var follower = require("../models/follower"),
 exports.statcodes = {
   _400: JSON.stringify({response:"bad request"}),
   _401: JSON.stringify({response:"unauthorized"}),
+  _404: JSON.stringify({response: "not found"}),
   _409: JSON.stringify({response:"new user not created"}),
   _500: JSON.stringify({response:"server error"})
 };
