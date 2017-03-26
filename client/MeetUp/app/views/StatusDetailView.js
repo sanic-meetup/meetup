ce15f0623f20f4542be23b63d26236a1f6a31535
Daniel Persaud <dev.persaud@mail.utoronto.ca>
2017-03-26 13:10:04 -0400

    added some sample map code

--- client/MeetUp/app/views/StatusDetailView.js
+++ client/MeetUp/app/views/StatusDetailView.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import {MapStyle} from  "../Constants";
+// var MapView = require('react-native-maps');
 
 export default class StatusDetailView extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+        region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 100,
+        longitudeDelta: 100,
+      }
+    }
+  }
+
+  onRegionChange(region) {
+    this.setState({ region });
   }
 
   render() {
@@ -11,6 +26,12 @@ export default class StatusDetailView extends Component {
       <View>
         <Text>Status Detail View</Text>
         <Text>{this.props.username}</Text>
+        <MapView
+          region={this.state.region}
+          provider={PROVIDER_GOOGLE}
+          onRegionChange={this.onRegionChange}
+          customMapStyle={this.mapStyle}
+        />
       </View>
     );
   }
