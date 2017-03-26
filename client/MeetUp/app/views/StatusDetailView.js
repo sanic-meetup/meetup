485b1da27c948c1423453d1b15746ed05fa7c0da
Brandon Mowat <brandonmowat11@gmail.com>
2017-03-26 13:29:36 -0400

    Added react-native-maps

--- client/MeetUp/app/views/StatusDetailView.js
+++ client/MeetUp/app/views/StatusDetailView.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
+var MapView = require('react-native-maps');
+
 export default class StatusDetailView extends Component {
   constructor(props) {
     super(props);
@@ -11,6 +13,19 @@ export default class StatusDetailView extends Component {
       <View>
         <Text>Status Detail View</Text>
         <Text>{this.props.username}</Text>
+        <MapView
+          style={{
+            height:100,
+            width: 100
+          }}
+          region={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
+          }}
+          onRegionChange={this.onRegionChange}
+        />
       </View>
     );
   }
